9fc1e592133cc7913560fa681b360b9f1e18e978
Josie Lipkin <jlipkin8@gmail.com>
2023-02-08 11:22:34 -0500

    GuessResults Component

---
+++ src/components/GuessResults/GuessResults.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Guess from "../Guess/Guess";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants.js"
+import {range} from "../../utils";
+
+function GuessResults({guessList}) {
+  console.log("guessList", guessList)
+  return (
+    <div className="guess-results">
+      {
+        range(1, 1 + NUM_OF_GUESSES_ALLOWED).map((item) => {
+          return <Guess key={item} word={guessList[item]?.checked_word}></Guess>
+        })
+      }
+    </div>
+  );
+}
+
+export default GuessResults;
