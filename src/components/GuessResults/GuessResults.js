5870cbf60147a9d32a6bb73b6df08b9b512a1464
Josie Lipkin <jlipkin8@gmail.com>
2023-02-08 11:56:41 -0500

    Change variable name.

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -6,8 +6,10 @@ import { range } from "../../utils";
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {range(1, 1 + NUM_OF_GUESSES_ALLOWED).map((item) => {
-        return <Guess key={item} word={guessList[item]?.checked_word}></Guess>;
+      {range(NUM_OF_GUESSES_ALLOWED).map((index) => {
+        return (
+          <Guess key={index} word={guessList[index]?.checked_word}></Guess>
+        );
       })}
     </div>
   );
